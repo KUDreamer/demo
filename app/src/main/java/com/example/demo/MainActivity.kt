dd269ed1494861b9c9cab57e6d79a993474b1e35
Lee Ji Won <01dlwldnjs@gmail.com>
2024-05-18 21:44:00 +0900

    main items

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.demo.screens.detailedScheduleMain
 import com.example.demo.ui.theme.DemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    /*
                     val navController = rememberNavController()
                     NavGraph(navController, NavViewModel())
+
+                     */
+                    // for test
+                    detailedScheduleMain()
                 }
             }
         }
